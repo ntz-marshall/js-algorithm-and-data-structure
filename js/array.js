e41df30665eb846fec14d4cd77202e50f3df25e9
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-01-12 15:04:34 -0300

    Iniciando curso de JS: Algorithm & Data Structure pela Udemy parte do Project 2 Weeks

---
+++ js/array.js
@@ -0,0 +1,14 @@
+var avgTemp = [];
+
+avgTemp[0] = 31.9;
+avgTemp[1] = 35.3;
+avgTemp[2] = 42;
+avgTemp[3] = 38;
+avgTemp[4] = 25.5;
+
+console.log(avgTemp[0]);
+
+var daysOfWeek = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+// outra forma de declarar um array;
+
+console.log(daysOfWeek);
